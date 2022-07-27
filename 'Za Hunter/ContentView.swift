518cd7bc7bddb043b198878ddd952405af50f314
Ethan McHugh <ethanmchugh2024@gmail.com>
2022-07-27 11:23:30 -0500

    added map search feature

--- 'Za Hunter/ContentView.swift
+++ 'Za Hunter/ContentView.swift
@@ -10,6 +10,9 @@ import MapKit
 
 
 struct ContentView: View {
+    @State private var places = [Place]()
+    @StateObject var locationManager = LocationManager()
+    @State private var userTrackingMode: MapUserTrackingMode = .follow
     @State private var region = MKCoordinateRegion(
             center: CLLocationCoordinate2D(
                 latitude: 42.0558,
@@ -19,7 +22,32 @@ struct ContentView: View {
                 longitudeDelta: 0.05)
         )
     var body: some View {
-        Map(coordinateRegion: $region)
+        Map(coordinateRegion: $region,
+            interactionModes: .all,
+            showsUserLocation: true,
+            userTrackingMode: $userTrackingMode, annotationItems: places){ place in
+            MapPin(coordinate: place.annotation.coordinate)
+        }
+            .onAppear(){
+                performSearch(item: "pizza")
+            }
+        
+    }
+    func performSearch(item: String){
+        let searchRequest = MKLocalSearch.Request()
+        searchRequest.naturalLanguageQuery = item
+        searchRequest.region = region
+        let search = MKLocalSearch(request: searchRequest)
+        search.start { (response, error) in
+            if let response = response {
+                for mapItem in response.mapItems {
+                    let annotation = MKPointAnnotation()
+                    annotation.coordinate = mapItem.placemark.coordinate
+                    annotation.title = mapItem.name
+                    places.append(Place(annotation: annotation, mapItem: mapItem))
+                }
+            }
+        }
     }
 }
 
@@ -27,4 +55,10 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
+}
+
+struct Place: Identifiable {
+    let id = UUID()
+    let annotation: MKPointAnnotation
+    let mapItem: MKMapItem
 }
