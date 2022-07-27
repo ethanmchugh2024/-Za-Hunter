7f759a5f7814dd04babe132d51f09ebfb5aef78d
Ethan McHugh <ethanmchugh2024@gmail.com>
2022-07-27 10:57:40 -0500

    Initial Commit

---
+++ 'Za Hunter/_Za_HunterApp.swift
@@ -0,0 +1,17 @@
+//
+//  _Za_HunterApp.swift
+//  'Za Hunter
+//
+//  Created by Ethan McHugh on 7/27/22.
+//
+
+import SwiftUI
+
+@main
+struct _Za_HunterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
